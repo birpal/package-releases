4fb3057875d80bfd5a0adeb1e76644af519df434
Birkir Pálmason @birkir <birkirpalma@gmail.com>
2018-04-26 12:03:01 +0000

    feat(square-component): first release of the square component
    affects: @birpal/circle, @birpal/square
    
    The square component has sharp edges unlike the circle component. Which is round

---
+++ packages/square/src/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable */
+export { default as Square } from './Square';
---
+++ packages/square/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const pkg = require('./package.json');
+
+module.exports = {
+  entry: './src/index.js',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    library: pkg.name,
+    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, './', 'dist'),
+    filename: 'index.js',
+    auxiliaryComment: `${pkg.version}`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        use: { loader: 'babel-loader' },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { modules: true } },
+          { loader: 'sass-loader' },
+        ],
+      },
+    ],
+  },
+};
